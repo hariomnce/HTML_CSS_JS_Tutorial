3b70a1c758b7d4f0b6b8d63e6c83777ae69522db
Hariom Sharma <hariomne@gmail.com>
2023-09-08 11:59:14 +0530

    js folder added.

--- JS/demo.js
+++ JS/demo.js
@@ -1,6 +1,14 @@
-let namasteBtn=document.querySelector('button');
-namasteBtn.addEventListener('click', showMsg);
+// let namasteBtn=document.querySelector('button');
+// namasteBtn.addEventListener('click', showMsg);
 
-function showMsg(){
-    alert("Namaste World !");
+// function showMsg(){
+//     alert("Namaste World !");
+// }
+
+let namasteBtn = document.querySelector("button");
+namasteBtn.addEventListener("click", inputMsg);
+
+function inputMsg() {
+  let name = prompt("Enter Name of Student");
+  namasteBtn.textContent = "Roll No. 1:" + name;
 }
